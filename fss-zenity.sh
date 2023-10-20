8e619d1e9da6678a3fe85ba1ac6265d760d3f5e5
Jeremy Wininger <jdwininger@gmail.com>
2023-10-19 20:54:49 -0500

    even more work on zenity

--- fss-zenity.sh
+++ fss-zenity.sh
@@ -14,6 +14,15 @@ fi
 
 echo $sudo_password | sudo -S dnf install zenity
 
+reboot_or_return() {
+    zenity --question --title="Reboot or Return to Main Menu" --text="Would you like to reboot or return to the main menu?" --ok-label="Reboot" --cancel-label="Return to Main Menu"
+    if [ $? -eq 0 ]; then
+        sudo reboot
+    else
+        mainmenu
+    fi
+}
+
 show_progress() {
     local task_name=$1
     local total_steps=$2
@@ -44,8 +53,8 @@ updates() {
 	echo "Now updating your Fedora install. You will be asked for your password to continue. When this finishes your computer will reboot, so make sure all your work is saved. When your computer reboots run this script again."
 	echo ""
 	read -n 1 -s -p 'Press any key to continue.'
-	sudo dnf upgrade -y
-#	reboot -n
+	echo $sudo_password | sudo -S dnf upgrade -y
+	reboot_or_return
 }
 
 uninstall() {
@@ -53,68 +62,59 @@ uninstall() {
 	sudo dnf remove libreoffice* rhythmbox gnome-abrt mediawriter -y
 	clear
 	echo "Finished uninstalling unwanted Fedora applications."
-	echo ""
-        read -n 1 -s -p 'Press any key to return to the main menu'
-        clear
+	zenity --info --text="Finished uninstalling unwanted applications" --ok-label="Return to Main Menu"
 	mainmenu
 }
 
 flathub() {
-	sudo flatpak install https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref 
-	sudo flatpak install --system flathub org.inkscape.Inkscape -y
-	sudo flatpak install --system flathub com.discordapp.Discord -y
-	sudo flatpak install --system flathub org.videolan.VLC -y
-	sudo flatpak install --system flathub org.kde.krita -y
-	sudo flatpak install --system flathub org.blender.Blender -y
-	sudo flatpak install --system flathub com.github.libresprite.LibreSprite -y
-	sudo flatpak install --system flathub com.orama_interactive.Pixelorama -y
-	sudo flatpak install --system flathub org.tuxpaint.Tuxpaint -y
-	sudo flatpak install --system flathub org.upscayl.Upscayl -y
-	sudo flatpak install --system flathub org.atheme.audacious -y
-	sudo flatpak install --system flathub net.retrodeck.retrodeck -y
-	sudo flatpak install --system flathub org.ryujinx.Ryujinx -y
-	sudo flatpak install --system flathub org.yuzu_emu.yuzu -y
-	sudo flatpak install --system flathub com.obsproject.Studio -y
-	sudo flatpak install --system flathub io.github.antimicrox.antimicrox -y
-	sudo flatpak install --system flathub com.fightcade.Fightcade -y
-	sudo flatpak install --system flathub com.mattjakeman.ExtensionManager -y
-	sudo flatpak install --system flathub dev.gbstudio.gb-studio -y
-	sudo flatpak install --system flathub org.mapeditor.Tiled -y
-	sudo flatpak install --system flathub net.lutris.Lutris -y
-	sudo flatpak install --system flathub com.heroicgameslauncher.hgl -y
-	sudo flatpak install --system flathub com.github.tchx84.Flatseal -y
-	sudo flatpak install --system flathub org.kde.kdenlive -y
-	sudo flatpak install --system flathub fr.handbrake.ghb -y
-	sudo flatpak install --system flathub org.jdownloader.JDownloader -y
-	sudo flatpak install --system flathub com.github.Matoking.protontricks -y
-	sudo flatpak install --system flathub com.transmissionbt.Transmission -y
-	sudo flatpak install --system flathub com.vysp3r.ProtonPlus -y
-	sudo flatpak install --system flathub org.gnome.FileRoller -y
-	clear
-	echo "Finished installing Flathub applications. You'll need to log out and back in to see applications in gnome applications list."
-	echo ""
-        read -n 1 -s -p 'Press any key to return to the main menu'
-        clear
+	show_progress "Install Flathub applications - This takes a while" 30
+	echo $sudo_password | sudo -S flatpak install https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref 
+	echo $sudo_password | sudo -S flatpak install --system flathub org.inkscape.Inkscape -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.discordapp.Discord -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.videolan.VLC -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.kde.krita -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.blender.Blender -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.github.libresprite.LibreSprite -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.orama_interactive.Pixelorama -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.tuxpaint.Tuxpaint -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.upscayl.Upscayl -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.atheme.audacious -y
+	echo $sudo_password | sudo -S flatpak install --system flathub net.retrodeck.retrodeck -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.ryujinx.Ryujinx -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.yuzu_emu.yuzu -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.obsproject.Studio -y
+	echo $sudo_password | sudo -S flatpak install --system flathub io.github.antimicrox.antimicrox -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.fightcade.Fightcade -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.mattjakeman.ExtensionManager -y
+	echo $sudo_password | sudo -S flatpak install --system flathub dev.gbstudio.gb-studio -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.mapeditor.Tiled -y
+	echo $sudo_password | sudo -S flatpak install --system flathub net.lutris.Lutris -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.heroicgameslauncher.hgl -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.github.tchx84.Flatseal -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.kde.kdenlive -y
+	echo $sudo_password | sudo -S flatpak install --system flathub fr.handbrake.ghb -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.jdownloader.JDownloader -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.github.Matoking.protontricks -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.transmissionbt.Transmission -y
+	echo $sudo_password | sudo -S flatpak install --system flathub com.vysp3r.ProtonPlus -y
+	echo $sudo_password | sudo -S flatpak install --system flathub org.gnome.FileRoller -y
+	zenity --info --text="Finished installing Flathub applications" --ok-label="Return to Main Menu"
 	mainmenu
 }
 
 rpmfusion() {
-	sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-	sudo dnf groupupdate core -y
-	sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
-	sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
-	sudo dnf groupupdate sound-and-video -y
-	sudo dnf install rpmfusion-free-release-tainted -y
-	sudo dnf install libdvdcss -y
-	sudo dnf install steam wine winetricks gnome-tweaks kernel-modules-extra -y
-	sudo dnf install fedora-workstation-repositories -y
-	sudo dnf config-manager --set-enabled google-chrome 
-	sudo dnf install google-chrome-stable -y
-	clear
-	echo "Finished installing Fedora & RPMfusion applications."
-	echo ""
-        read -n 1 -s -p 'Press any key to return to the main menu'
-        clear
+	echo $sudo_password | sudo -S dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+	echo $sudo_password | sudo -S dnf groupupdate core -y
+	echo $sudo_password | sudo -S dnf swap ffmpeg-free ffmpeg --allowerasing -y
+	echo $sudo_password | sudo -S dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+	echo $sudo_password | sudo -S dnf groupupdate sound-and-video -y
+	echo $sudo_password | sudo -S dnf install rpmfusion-free-release-tainted -y
+	echo $sudo_password | sudo -S dnf install libdvdcss -y
+	echo $sudo_password | sudo -S dnf install steam wine winetricks gnome-tweaks kernel-modules-extra -y
+	echo $sudo_password | sudo -S dnf install fedora-workstation-repositories -y
+	echo $sudo_password | sudo -S dnf config-manager --set-enabled google-chrome 
+	echo $sudo_password | sudo -S dnf install google-chrome-stable -y
+	zenity --info --text="Finshed installing Fedora packages" --ok-label="Return to Main Menu"
 	mainmenu
 }
 
@@ -135,16 +135,12 @@ tweaks() {
 	gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Utility/ categories "['Utility', 'X-GNOME-Utilities']"
 	gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Development/ categories "['Development']"
 	gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Network/ categories "['Network']"
-	clear
-	echo "Finished tweaking Gnome."
-	echo ""
-        read -n 1 -s -p 'Press any key to return to the main menu'
-        clear
+	zenity --info --text="Finished setting Gnome tweaks" --ok-label="Return to Main Menu"
 	mainmenu
 }
 
 # Function to display menu using zenity
-show_menu() {
+mainmenu() {
     zenity --forms \
         --title="Jeremys Fedora Setup Script" \
         --text="Please select an option" \
@@ -155,19 +151,31 @@ show_menu() {
 # Function to perform actions based on menu selection
 menu_actions() {
     case $1 in
-        "Option 1")
+        "Update this Fedora Install")
 			clear
             echo "Update this Fedora Install"
 			updates
             ;;
-        "Option 2")
+        "Uninstall unwanted applications")
 			clear
             echo "Uninstall unwanted applications"
 			uninstall			
             ;;
-        "Option 3")
-            echo "You selected Option 3"
-            ;;
+        "Setup RPMfusion and install Fedora applications|Install Flathub applications")
+			clear
+            echo "Setup RPMfusion and install Fedora applications|Install Flathub applications"
+            rpmfusion
+			;;
+		"Install Flathub applications")
+			clear
+			echo "Install Flathub applications"
+			flathub
+			;;
+		"Set system tweaks")
+			clear
+			echo "Set system tweaks"
+			tweaks
+			;;
         "Exit")
             exit 0
             ;;
@@ -180,7 +188,7 @@ menu_actions() {
 # Main loop
 while true; do
     # Display menu and get user selection
-    selected_option=$(show_menu)
+    selected_option=$(mainmenu)
 
     # Perform actions based on user selection
     menu_actions "$selected_option"
